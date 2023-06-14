7ea727a726544237b6d9a1ca33252d139575184e
HungTran98 <hungtranitvn@gmail.com>
2023-06-15 00:22:51 +0700

    validate login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import './Login.scss'
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
+const schemaLogin = Yup.object({
+  email: Yup.string().email().required('Email is required'),
+  passWord: Yup.string().required('passWord is required').min(6, 'Must be at least 2 characters').max(15, 'Must be at least 15 characters')
+});
 function Login() {
+  const navigate = useNavigate()
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      passWord: ''
+    },
+    validationSchema: schemaLogin,
+
+    onSubmit: async (values) => {
+      console.log(values)
+      try {
+        const resp = await axios.post('https://shop.cyberlearn.vn/api/Users/signin', {
+          "email": values.email,
+          "password": values.passWord,
+        })
+
+        navigate('/profile')
+        console.log(resp)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
   return (
-    <div>Login</div>
+    <div className='login'>
+      <h2>Login</h2>
+      <hr style={{ width: '84%', border: '1px solid #DEDDDC', marginBottom: '5.2rem', margin: 'auto' }} />
+      <form onSubmit={formik.handleSubmit} style={{ margin: 'auto', width: '28%', paddingTop: '2.7rem' }}>
+        <div className="login_content">
+          <label>Email</label>
+          <input placeholder='email' name='email' type='text' {...formik.getFieldProps('email')} />
+          {formik.errors.email && formik.touched.email && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.email}</p>}
+        </div>
+        <div className="login_content">
+          <label>Password</label>
+          <input placeholder='passWord' name='passWord' type='text' {...formik.getFieldProps('passWord')} />
+          {formik.errors.passWord && formik.touched.passWord && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.passWord}</p>}
+        </div>
+        <div className="login_submit" style={{ display: 'flex', justifyContent: 'flex-end', paddingBottom: '4rem' }}>
+          <p>Register now ?</p>
+          <button type='submit'>Login</button>
+        </div>
+
+      </form>
+    </div>
   )
 }
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,84 +2,97 @@ import React from 'react'
 import './Register.scss'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const schemaRegister = Yup.object({
-  email: Yup.string().required('Email is required'),
+  email: Yup.string().email().required('Email is required'),
   passWord: Yup.string().required('passWord is required').min(6, 'Must be at least 2 characters').max(15, 'Must be at least 15 characters'),
-  confirmPassWord: Yup.string().required('confirmPassWord is required').min(6, 'Must be at least 2 characters').max(15, 'Must be at least 15 characters'),
+  confirmPassWord: Yup.string().required('confirmPassWord is required').min(6, 'Must be at least 6 characters').max(15, 'Must be at least 15 characters').oneOf([Yup.ref('passWord')], 'Confirm PassWord must be matched'),
   name: Yup.string().required('name is required').max(15, 'Must be at least 15 characters'),
-  phone: Yup.string().required('phone is required').max(15, 'Must be at least 15 characters'),
+  phone: Yup.string().required('phone is required').min(10, 'Must be at least 10 characters').max(10, 'Must be at least 10 characters'),
 });
 function Register() {
 
+  const navigate = useNavigate()
   const formik = useFormik({
     initialValues: {
       email: '',
       passWord: '',
-      confirmPassWord: '',
       name: '',
       phone: '',
-      male: '',
-      femail: ''
+      gender: false
     },
 
     validationSchema: schemaRegister,
 
-    onSubmit: (values) => {
-      console.log(values)
+    onSubmit: async (values) => {
+      try {
+        const resp = await axios.post('https://shop.cyberlearn.vn/api/Users/signup', {
+          "email": values.email,
+          "password": values.passWord,
+          "name": values.name,
+          "gender": values.gender,
+          "phone": values.phone
+        })
+        navigate('/login')
+        console.log(resp)
+        
+      } catch (error) {
+        console.log(error)
+      }
     }
   })
   return (
     <div className='register'>
       <h2>Register</h2>
-      <hr style={{ width: '80%', border: '1px solid #DEDDDC', marginBottom: '5.2rem', margin: 'auto' }} />
+      <hr style={{ width: '84%', border: '1px solid #DEDDDC', marginBottom: '5.2rem', margin: 'auto' }} />
       <form onSubmit={formik.handleSubmit} style={{ paddingTop: '5.2rem' }}>
         <div className="register_left">
           <div className="register_content">
             <label>Email</label>
-            <input placeholder='email' name='email' {...formik.getFieldProps('email')} />
-            {formik.errors.email && formik.touched.email && <p>{formik.errors.email}</p>}
+            <input placeholder='email' name='email' type='text' {...formik.getFieldProps('email')} />
+            {formik.errors.email && formik.touched.email && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.email}</p>}
           </div>
 
           <div className="register_content">
             <label>Password</label>
-            <input placeholder='passWord' name='passWord' {...formik.getFieldProps('passWord')} />
-            {formik.errors.passWord && formik.touched.passWord && <p>{formik.errors.passWord}</p>}
+            <input placeholder='passWord' name='passWord' type='text' {...formik.getFieldProps('passWord')} />
+            {formik.errors.passWord && formik.touched.passWord && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.passWord}</p>}
           </div>
           <div className="register_content">
             <label>Password confirm</label>
-            <input placeholder='password confirm' name='confirmPassWord' {...formik.getFieldProps('confirmPassWord')} />
-            {formik.errors.confirmPassWord && formik.touched.confirmPassWord && <p>{formik.errors.confirmPassWord}</p>}
+            <input placeholder='password confirm' name='confirmPassWord' type='text' {...formik.getFieldProps('confirmPassWord')} />
+            {formik.errors.confirmPassWord && formik.touched.confirmPassWord && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.confirmPassWord}</p>}
 
           </div>
         </div>
         <div className="register_right">
           <div className="register_content">
             <label>Name</label>
-            <input placeholder='name' name='name' {...formik.getFieldProps('name')} />
-            {formik.errors.name && formik.touched.name && <p>{formik.errors.name}</p>}
+            <input placeholder='name' name='name' type='text' {...formik.getFieldProps('name')} />
+            {formik.errors.name && formik.touched.name && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.name}</p>}
 
           </div>
           <div className="register_content">
             <label>Phone</label>
-            <input placeholder='phone' name='phone' {...formik.getFieldProps('phone')} />
-            {formik.errors.phone && formik.touched.phone && <p>{formik.errors.phone}</p>}
+            <input placeholder='phone' name='phone' type='text' {...formik.getFieldProps('phone')} />
+            {formik.errors.phone && formik.touched.phone && <p style={{ color: '#F50E0E', fontFamily: 'Roboto', fontWeight: 400, fontSize: '1.2rem' }}>{formik.errors.phone}</p>}
 
           </div>
-          <div class="register_radio">
+          <div class="register_radio" {...formik.getFieldProps('gender')}>
             <label>Gender</label>
-            <div className="radio_button">
-              <input type="radio" name='male' value='male' onChange={formik.handleChange} onBlur={formik.handleBlur} />
+            <div className="radio_button" >
+              <input type="radio" name='gender' value={true} />
               <label>Male</label>
             </div>
-            <div className="radio_button">
-              <input type="radio" name='femail' value='femail' onChange={formik.handleChange} onBlur={formik.handleBlur} />
+            <div className="radio_button"  >
+              <input type="radio" name='gender' value={false} />
               <label>Femail</label>
             </div>
-          </div>
+          </div >
           <button type='submit'>Submit</button>
         </div>
-
       </form>
 
     </div>
